ac94bfc30e467fb47f8044c81ebd79829e05755c
yikeJason <18215513386@163.com>
2024-05-19 21:49:51 +0800

    feature support DashMap

--- examples/metrics.rs
+++ examples/metrics.rs
@@ -12,7 +12,7 @@ fn main() -> Result<()> {
 
     // start N workers and M requesters
 
-    println!("{:?}", metrics.snapshot());
+    println!("{:?}", metrics);
 
     for idx in 0..N {
         task_worker(idx, metrics.clone())?; // Metrics {data: Arc::clone(&metrics.data)}
